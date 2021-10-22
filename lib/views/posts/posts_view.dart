baa463888b8795a13f92b60d16d90e476214cdf6
Mohamed Fathy <codetwostart@gmail.com>
2021-10-22 09:55:37 +0200

    lesson 6

--- lib/views/posts/posts_view.dart
+++ lib/views/posts/posts_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_api_ar/repositories/posts/posts_api.dart';
 import 'package:mvvm_api_ar/repositories/posts/posts_fake.dart';
+import 'package:mvvm_api_ar/repositories/posts/posts_local.dart';
+import 'package:mvvm_api_ar/view_models/post_view_model.dart';
 import 'package:mvvm_api_ar/view_models/posts_view_model.dart';
 
 class PostsView extends StatelessWidget {
@@ -15,10 +17,20 @@ class PostsView extends StatelessWidget {
         title: Text(postsViewModel.title),
       ),
       body: Center(
-        child: TextButton(
-          child: Text('Load data'),
-          onPressed: () {
-            postsViewModel.fetchAllPosts();
+        child: FutureBuilder<List<PostViewModel>>(
+          future: postsViewModel.fetchAllPosts(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else {
+              var posts = snapshot.data;
+              return ListView.builder(
+                itemCount: posts?.length,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(posts![index].title),
+                ),
+              );
+            }
           },
         ),
       ),
